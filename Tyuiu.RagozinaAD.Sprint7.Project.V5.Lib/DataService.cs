e60c778fcd779d11b18b31d59bfac58968796f83
Алина <@DESKTOP-P40RFID>
2025-12-16 20:50:23 +0500

    Add project files.

---
+++ Tyuiu.RagozinaAD.Sprint7.Project.V5.Lib/DataService.cs
@@ -0,0 +1,91 @@
+﻿namespace Tyuiu.RagozinaAD.Sprint7.Project.V5.Lib
+{
+    public class DataService
+    {
+        public string[,] GetMatrix(string path)
+        {
+            string[] str = File.ReadAllLines(path);
+
+            int columns = str[0].Split(';').Length;
+            int rows = str.Length;
+
+            string[,] matrix = new string[rows, columns];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                string strIndexI = str[i];
+                string[] strArr = strIndexI.Split(';');
+                for (int j = 0; j < strArr.Length; j++)
+                {
+                    matrix[i, j] = strArr[j];
+                }
+            }
+            return matrix;
+        }
+
+        public string[,] SortMax(string[,] matrix, int NumberColumn)
+        {
+            int[] col = new int[matrix.GetLength(0) - 1];
+            col[col.Length - 1] = Convert.ToInt32(matrix[matrix.GetLength(0) - 1, NumberColumn]);
+            for (int i = 0; i < col.Length - 1; i++)
+            {
+                col[i] = Convert.ToInt32(matrix[i + 1, NumberColumn]);
+            }
+
+            Array.Sort(col, (x, y) => y.CompareTo(x));
+
+            string[,] Sort = new string[matrix.GetLength(0), matrix.GetLength(1)];
+
+            for (int i = 0; i < Sort.GetLength(1); i++)
+            {
+                Sort[0, i] = matrix[0, i];
+            }
+
+            for (int i = 0; i < Sort.GetLength(0) - 1; i++)
+            {
+                for (int j = 1; j < matrix.GetLength(0); j++)
+                {
+                    if (col[i] == Convert.ToInt32(matrix[j, NumberColumn]))
+                    {
+                        for (int c = 0; c < Sort.GetLength(1); c++)
+                        {
+                            Sort[i + 1, c] = matrix[j, c];
+                        }
+                        matrix[j, NumberColumn] = "-1";
+                        break;
+                    }
+                }
+            }
+            return Sort;
+        }
+        public string[,] Search(string[,] table, string n)
+        {
+            int s = 0;
+            for (int i = 0; i < table.GetLength(0); i++)
+            {
+                if (table[i, 1] == n)
+                {
+                    s++;
+                }
+            }
+
+            string[,] res = new string[s, 4];
+            int c = 0;
+
+            for (int i = 0; i < table.GetLength(0); i++)
+            {
+                if (table[i, 1] == n)
+                {
+                    for (int r = 0; r < res.GetLength(1); r++)
+                    {
+                        res[c, r] = table[i, r];
+                    }
+                    table[i, 1] = "";
+                    c++;
+                }
+            }
+            return res;
+        }
+    }
+}
+
---
+++ Tyuiu.RagozinaAD.Sprint7.Project.V5.Test/DataServiceTest.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.RagozinaAD.Sprint7.Project.V5.Lib;
+namespace Tyuiu.RagozinaAD.Sprint7.Project.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void SortMax()
+        {
+            DataService ds = new DataService();
+            string[,] tab =
+            {
+                {"Код","Название","Количество","Стоимость" },
+                {"1","Ручка шариковая","300","20"},
+                {"2","Бумага офисная","220","300"},
+                {"3","Скотч","150","90"},
+                {"4","Блокнот","130","150"}
+            };
+
+            string[,] res = ds.SortMax(tab , 3);
+            string[,] wait =
+            {
+                {"Код","Название","Количество","Стоимость" },
+                {"2","Бумага офисная","220","300"},
+                {"4","Блокнот","130","150"},
+                {"3","Скотч","150","90"},
+                {"1","Ручка шариковая","300","20"},
+            };
+            CollectionAssert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void TestSearch()
+        {
+            DataService ds = new DataService();
+            string[,] tab =
+            {
+                {"Код","Название","Количество","Стоимость" },
+                {"1","Ручка шариковая","300","20"},
+                {"2","Бумага офисная","220","300"},
+                {"3","Скотч","150","90"},
+                {"4","Блокнот","130","150"}
+            };
+
+            string[,] res = ds.Search(tab, "Бумага офисная");
+            string[,] wait =
+            {
+                {"2","Бумага офисная","220","300"},
+            };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
+
