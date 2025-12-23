68a36560d832aae19569da00c6d20a1c961960d8
Алина <@DESKTOP-P40RFID>
2025-12-23 16:07:07 +0500

    Sprint

--- Tyuiu.RagozinaAD.Sprint7.Project.V5/FormMain.cs
+++ Tyuiu.RagozinaAD.Sprint7.Project.V5/FormMain.cs
@@ -30,11 +30,5 @@ namespace Tyuiu.RagozinaAD.Sprint7.Project.V5
             FormPost formPost = new FormPost();
             formPost.ShowDialog();
         }
-
-        private void buttonChart_Click(object sender, EventArgs e)
-        {
-            FormChart formChart = new FormChart();
-            formChart.ShowDialog();
-        }
     }
 }
